45463720f089badd3a95de8d551b9c13c60448d7
amm0theman <ammonriley.97@gmail.com>
2018-11-05 10:12:17 -0700

    part 2

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 using Avalonia.Controls;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace cooljam.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Hello World!";
+        IWebAPI web = new WebAPI();
+        Task<HttpResponseMessage> a;
+        public string Greeting => getstats();
+
+        public string getstats() {
+            var k = this.web.GetPlayerFromAsync("ApRiL").Result.ToString();
+            return k;
+        }
     }
 }
